09551d8dc29526d0d2409d8791043887643ea83e
AlisaKravchenko <75129763+AlisaKravchenko@users.noreply.github.com>
2022-03-20 18:29:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-// const express = require('express')
-// const bodyParser = require('body-parser')
+const express = {
+a: 1,
+b: 2,
+c: {d: 4}
+}
 
-// const app = express()
-// app.use(bodyParser.json())
-
-// const products = [
-//     {
-//         id: 1,
-//         name: 'phone',
-//         price: 300
-//     },{
-//         id: 2,
-//         name: 'tablet',
-//         price: 700
-//     }
-// ]
-
-// app.get('/products', (req, res) => res.json(products))
-
-
-// app.listen(3000, () => console.log('listening on 3000!'))
-`${
-    {a: 1, b: 2, c: {d: 4}}
-
-}`
