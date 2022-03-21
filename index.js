4b3f3495f7f04ff64d77ef8661e1b20168a62d5c
AlisaKravchenko <75129763+AlisaKravchenko@users.noreply.github.com>
2022-03-21 16:54:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-{
-  words: ['a\n', '\nabandon\n', '\nability\n', '\nable\n', '\nabolish\n', '\nabortion\n', '\nabout\n', '\nabove\n', '\n\n\nabroad\n', '\nabsence\n', '\nabsent\n', '\nabsolute\n', '\nabsolutely\n', '\nabsorb\n', '\nabstract\n', '\nabsurd\n', '\nabundance\n', '\nabuse\n', '\n\n\nabuse\n', '\nacademic\n', '\nacademy\n', '\naccelerate\n', '\naccent\n', '\naccept\n', '\naccep']
-
-}
+["a\n","\nabandon\n","\nability\n","\nable\n","\nabolish\n","\nabortion\n","\nabout\n","\nabout\n","\nabove\n","\nabove\n","\n\n\nabroad\n","\nabsence\n","\nabsent\n","\nabsolute\n","\nabsolutely\n","\nabsorb\n","\nabstract\n","\nabsurd\n","\nabundance\n","\nabuse"]
